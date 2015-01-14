3f21098ca0c6da0719b167dd2cd530e370e3794d
Pei Sun <peisun01@gmail.com>
2015-01-14 09:34:41 -0500

    First commit.

---
+++ Udemy9 To Do List/FirstViewController.swift
@@ -0,0 +1,85 @@
+//
+//  FirstViewController.swift
+//  Udemy9 To Do List
+//
+//  Created by Pei Sun on 2014-12-26.
+//  Copyright (c) 2014 psun. All rights reserved.
+//
+
+import UIKit
+
+var toDoItems:[String] = []
+    //This var available to the secondViewController since it's outside the FirstViewController class
+
+class FirstViewController: UIViewController, UITableViewDelegate{
+    
+    //For some reason, setting the constraints of the TableView to -16 Left and 0 Right makes the lines equidistant from the sides
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoItems.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        //NOTE: func tableView does run code inside if array toDoItems is empty (i.e. when user first opens the app, whether or something has been stored). 
+            //Therefore, we cannnot load from storedToDoItems from here. We can either load it in 
+            //(1)      viewDidload at the first load of the app,
+            //(2)      or we can load is everytime the page is refreshed in viewWillAppear
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        cell.textLabel.text = toDoItems[indexPath.row]
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //if storedToDoItems is empty (i.e. the first time user opens the app), then don't do anything with storedToDoItems. Though it doesn't seem to crash without the 'if', doing things with 'nil' are prone to crashes
+        //NOTE: We cannot do   toDoItems = storedToDoItems   b/c toDoItems is an array, storedToDoItems is an object
+        //NOTE: remove the "?" after AnyObject? suggested in the fit-it
+        
+        if let storedToDoItems: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("toDoList"){
+            toDoItems = []
+            toDoItems = storedToDoItems as [String]
+            
+            /*  //Could also do this, but the line above is less likely to crash
+            for var i = 0; i < storedToDoItems.count; ++i {
+                toDoItems.append(storedToDoItems[i] as NSString)
+            } */
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        
+        //If user tries to delete an item from a table, update toDoItems array and save into permanent storage
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            toDoItems.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(toDoItems, forKey: "toDoList")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+            tasksTable.reloadData() //Refreshes the table. Uses the @IBOutlet tasksTable below
+            
+        }
+    }
+
+    
+    //Manually type the line below. Don't forget to right click the table on main storyboard and assign 'referencing outlets' to tasksTable
+    @IBOutlet var tasksTable: UITableView!
+    
+    //-------- optional code for learning ---------
+
+    //If you want to refresh the table everytime the table is loaded, make IBOutlet var tasksTable & override func viewWillAppear
+    //Q: What is the difference beween load and appear?
+    //A: Loading happens just once when the app is first run. Appear is everytime the page is refreshed
+    override func viewWillAppear(animated: Bool) {
+        println("Running viewWillAppear")
+    }
+    //----------------------------------------------
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
---
+++ Udemy9 To Do List/SecondViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SecondViewController.swift
+//  Udemy9 To Do List
+//
+//  Created by Pei Sun on 2014-12-26.
+//  Copyright (c) 2014 psun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var getText: UITextField!
+    
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        toDoItems.append(getText.text)
+        NSUserDefaults.standardUserDefaults().setObject(toDoItems, forKey: "toDoList")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        self.view.endEditing(true)
+    }
+    
+    //Allow return key to resign keyboard. Remember to delegate the text field "getText" to the view controller
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        getText.resignFirstResponder()
+        return true
+    }
+    
+    //can just start typing "touchesBegan" and it'll auto add the override code
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
